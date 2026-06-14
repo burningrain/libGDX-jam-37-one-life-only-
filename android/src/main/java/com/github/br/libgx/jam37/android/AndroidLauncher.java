c37797cfbdcd1bd6c0ec2c39f0f6ecca73e5ee8a
ash <lixkram@gmail.com>
2026-06-14 16:51:07 +0300

    init commit from GDX-Liftoff

---
+++ android/src/main/java/com/github/br/libgx/jam37/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.github.br.libgx.jam37.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.github.br.libgx.jam37.Main;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new Main(), configuration);
+    }
+}
